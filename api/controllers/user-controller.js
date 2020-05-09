91133d9e18ccd69d94d4c90bc15e336247bee37b
abesheknarayan <abeshek.ramanathan2000@gmail.com>
2020-05-09 19:03:32 +0530

    [Routes]: Added routes for all pages

--- api/controllers/user-controller.js
+++ api/controllers/user-controller.js
@@ -1,7 +1,48 @@
 const User = require('../models/User');
 const signale = require('signale');
+const spawn = require("child_process").spawn;
 
 
-exports.regUser = (req,res) => {
+exports.renderLoginform = (req,res) => {
+    res.render("login");
+}
+
+exports.loginUser = (req,res) => {
      // user registration function
+     let username=req.body.webmail;
+     let password= req.body.password;
+     signale.info(username);
+     signale.info(password);
+
+    const pythonProcess = spawn('python', ["./checkCredentialsWebmail.py", username, password]);
+    pythonProcess.stdout.on('data', (data) => {
+        // console.log(data.toString());
+        if (data.toString() == 0) {
+            signale.note("****Wrong Credentials!****");
+            return res.redirect('/login')
+        }
+        else {
+            signale.note("****Credentials are correct!****");
+            req.session.user = {
+                name: username
+            }
+            return res.redirect(`/user/game`);
+        }
+    })
+}
+
+exports.renderGame = (req,res) => {
+    res.render("Game");
+}
+
+exports.renderAfterGame = (req,res) => {
+    res.render('AfterGame');
+}
+
+exports.saveGame = (req,res) => {
+    signale.success("Game saved");
+}
+
+exports.renderLeaderboard = (req,res) => {
+    res.render('Leaderboard');
 }
--- api/routers/routes.js
+++ api/routers/routes.js
@@ -2,10 +2,33 @@ const express = require('express');
 const router = express.Router();
 const user = require('../controllers/user-controller');
 
-router.get('/status',(req,res)=>{
-    res.json({msg: "Api running"});
-})
+// sessionChecker middlewares
+let sessionChecker = (req, res, next) => {
+    if (!req.session.user) {
+      res.redirect('/login');
+    } else {
+      next()
+    }
+  }
 
-router.get('/regUser',user.regUser);
+
+
+
+// login
+
+router.post('/login',user.loginUser);
+
+// game
+router.get('/game',sessionChecker,user.renderGame);
+router.post('/game',sessionChecker,user.saveGame);
+
+// Completion Page
+router.get('/afterGame',sessionChecker,user.renderAfterGame);  // url isnt that great plis change
+
+// Scoreboard
+router.get('/leaderboard',sessionChecker,user.renderLeaderboard);
+
+// register
+// router.get('/regUser',user.regUser);
 
 module.exports = router;
