aef921efda5c3cd3b35bfdc6797dd54e9fe2b83e
abesheknarayan <abeshek.ramanathan2000@gmail.com>
2020-05-09 11:11:17 +0530

    [Db]: Initialize connection with db

--- api/routers/routes.js
+++ api/routers/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const user = require('../controllers/user-controller');
 
 router.get('/status',(req,res)=>{
     res.json({msg: "Api running"});
 })
+
+router.get('/regUser',user.regUser);
 
 module.exports = router;
--- index.js
+++ index.js
@@ -3,12 +3,28 @@ const path = require("path");
 const PORT = process.env.PORT || 8081;
 const bodyParser = require("body-parser");
 const api_routes = require("./api/routers/routes");
+const mongoose = require('mongoose');
+const signale = require('signale');
+const config = require("./config/config");
+const expressValidator = require('express-validator'); 
+
+// initializing express
 const app = express();
 //configure database 
+  
+mongoose.connect(config.mongodb.dbURI,config.mongodb.setting)
+  .then(() => {
+    signale.success('*****Database Connection Successfull******');
+  }).catch(err => {
+    signale.fatal(new Error(err));
+    signale.warn('Could not connect to Database. Exiting now...');
+    process.exit();
+  })
+let db = mongoose.connection;
 
 
 //configure body parser
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,5 +38,5 @@ app.get("/*", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+  signale.success(`Server is running on ${PORT}`);
 });
