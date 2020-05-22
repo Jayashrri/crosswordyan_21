5cdce60c2f765d953187aff37646a12da2a24efe
Avi Kumar Singh <avithewinner1508@gmail.com>
2020-05-22 05:30:56 +0000

    fix: fix mobile view and ellipsizing in pagination (#18)

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -1,4 +1,6 @@
-function buildLeaderboard(parentID, listPeople, n, m, pageNumber, itemsPerPage) {
+function buildLeaderboard(parentID, listPeople, n, m, pageNumber, itemsPerPage, pageNumberId) {
+
+    setCurrentPage(pageNumber, pageNumberId, n, itemsPerPage,parentID);
 
     var screenWidth = window.innerWidth;
     var parent = document.getElementById(parentID);
@@ -34,8 +36,8 @@ function buildLeaderboard(parentID, listPeople, n, m, pageNumber, itemsPerPage) 
         row = document.createElement('tr');
         for(let j=0; j<m; j++) {
             
-            td.insertAdjacentHTML('beforeend', ''+listPeople[i][j])+'';
             td = document.createElement('td');
+            td.insertAdjacentHTML('beforeend', ''+listPeople[i][j])+'';
 
             // for rounding the corners.
             if(j==0) {
@@ -65,28 +67,112 @@ function buildLeaderboard(parentID, listPeople, n, m, pageNumber, itemsPerPage) 
 
 }
 
-function buildPages(parentID, n, m, tableID, listPeople) {
+function setCurrentPage(pageNumber, pageNumberContainerId, n, itemsPerPage, tableID) {
+    
+    // pageNumber is in the erange of 0,1,...n-1 so incrementing it.
+    pageNumber++;
 
+    // for styling
     var screenWidth = window.innerWidth;
-    // number of items in a single page.
-    var itemsPerPage=10;
-    // number of boxes for pagination
-    var numberOfBoxes=Math.ceil(n/itemsPerPage);
+    
+    // total number of pages for pagination
+    var numberOfBoxes=Math.ceil((n)/itemsPerPage);
 
-    // load page 1 for the first time.
-    buildLeaderboard(tableID,listPeople,n,m,0,itemsPerPage);
+    // total number of boxes currently visible
+    // includes ... as a box since they have same dimensions.
+    // 7 is in the general case when 1,...,current-1,current,current+1,...,last.
+    var numberOfBoxesVisible=7;
 
-    var parent = document.getElementById(parentID);
+    var parent = document.getElementById(pageNumberContainerId);
+
+    // styling the container view so that it comes sin the middle.
+    parent.innerHTML="";
     parent.style.display='inline-flex';
     parent.style.height='30px';
     parent.style.textAlign='center';
-    parent.style.paddingLeft=((screenWidth/2-20*numberOfBoxes)+'px');
+    parent.style.paddingLeft=((screenWidth/2-20*numberOfBoxesVisible)+'px');
     parent.style.marginTop=('3%');
     parent.style.marginBottom=('2%');
 
     // depending upon the size of data, add boxes and set on click on them with the the box number.
-    for(var i=1; i<=numberOfBoxes; i++) {
-        parent.innerHTML+='<div style="width: 30px; height: 30px; background-color: #000000; color: #FFFFFF; vertical-align: middle; text-align: center; margin-left: 10px; line-height: 30px; cursor: pointer;" onclick=buildLeaderboard("'+tableID+'",listPeople,'+n+','+m+','+(i-1)+','+itemsPerPage+');>'+i+'</div>';
+
+    // in this case simply draw 6 boxes, else depending upon the current page.
+    if(numberOfBoxes<=5) {
+
+        // reseting number of boxes that are visible and based on that margin, to set them in the centre.
+        numberOfBoxesVisible=numberOfBoxes;
+        parent.style.paddingLeft=((screenWidth/2-20*numberOfBoxesVisible)+'px');
+
+        for(var i=1; i<=numberOfBoxes; i++) {
+            parent.innerHTML+='<div class="pageNumberBox" onclick=buildLeaderboard("'+tableID+'",listPeople,'+n+','+m+','+(i-1)+','+itemsPerPage+',"'+pageNumberContainerId+'");>'+i+'</div>';
+        }
+
+    } else {
+        
+        // always first box will appear so drawing them first.
+        parent.innerHTML+='<div class="pageNumberBox" onclick=buildLeaderboard("'+tableID+'",listPeople,'+n+','+m+','+0+','+itemsPerPage+',"'+pageNumberContainerId+'");>'+1+'</div>';
+
+        // in this case show first three boxes, then ... and then the last two boxes.
+        if(pageNumber==1) {
+
+            numberOfBoxesVisible=5;
+            parent.style.paddingLeft=((screenWidth/2-20*numberOfBoxesVisible)+'px');
+
+            parent.innerHTML+='<div class="pageNumberBox" onclick=buildLeaderboard("'+tableID+'",listPeople,'+n+','+m+','+1+','+itemsPerPage+',"'+pageNumberContainerId+'");>'+2+'</div>';
+            parent.innerHTML+='<div class="paginationDots">...</div>';
+            parent.innerHTML+='<div class="pageNumberBox" onclick=buildLeaderboard("'+tableID+'",listPeople,'+n+','+m+','+(numberOfBoxes-2)+','+itemsPerPage+',"'+pageNumberContainerId+'");>'+(numberOfBoxes-1)+'</div>';
+            parent.innerHTML+='<div class="pageNumberBox" onclick=buildLeaderboard("'+tableID+'",listPeople,'+n+','+m+','+(numberOfBoxes-1)+','+itemsPerPage+',"'+pageNumberContainerId+'");>'+numberOfBoxes+'</div>';
+        
+        } else {
+
+            if(pageNumber<=3) {
+
+                // draw first four boxes and then ... and then the last two boxes.
+                // else if pageNumber is 4, then box 1,2 followed by ... and then 4,5, then ... and finally last two boxes.
+                if(pageNumber==2) {
+
+                    numberOfBoxesVisible=5;
+                    parent.style.paddingLeft=((screenWidth/2-20*numberOfBoxesVisible)+'px');
+        
+                    parent.innerHTML+='<div class="pageNumberBox" onclick=buildLeaderboard("'+tableID+'",listPeople,'+n+','+m+','+1+','+itemsPerPage+',"'+pageNumberContainerId+'");>'+2+'</div>';
+                    parent.innerHTML+='<div class="pageNumberBox" onclick=buildLeaderboard("'+tableID+'",listPeople,'+n+','+m+','+2+','+itemsPerPage+',"'+pageNumberContainerId+'");>'+3+'</div>';
+                    parent.innerHTML+='<div class="paginationDots">...</div>';
+                    parent.innerHTML+='<div class="pageNumberBox" onclick=buildLeaderboard("'+tableID+'",listPeople,'+n+','+m+','+(numberOfBoxes-1)+','+itemsPerPage+',"'+pageNumberContainerId+'");>'+numberOfBoxes+'</div>';
+                } else if(pageNumber==3) {
+
+                    parent.innerHTML+='<div class="paginationDots">...</div>';
+                    parent.innerHTML+='<div class="pageNumberBox" onclick=buildLeaderboard("'+tableID+'",listPeople,'+n+','+m+','+2+','+itemsPerPage+',"'+pageNumberContainerId+'");>'+3+'</div>';
+                    parent.innerHTML+='<div class="pageNumberBox" onclick=buildLeaderboard("'+tableID+'",listPeople,'+n+','+m+','+3+','+itemsPerPage+',"'+pageNumberContainerId+'");>'+4+'</div>';
+                    parent.innerHTML+='<div class="pageNumberBox" onclick=buildLeaderboard("'+tableID+'",listPeople,'+n+','+m+','+4+','+itemsPerPage+',"'+pageNumberContainerId+'");>'+5+'</div>';
+                    parent.innerHTML+='<div class="paginationDots">...</div>';
+                    parent.innerHTML+='<div class="pageNumberBox" onclick=buildLeaderboard("'+tableID+'",listPeople,'+n+','+m+','+(numberOfBoxes-2)+','+itemsPerPage+',"'+pageNumberContainerId+'");>'+(numberOfBoxes-1)+'</div>';
+                }
+            } else if(pageNumber>=numberOfBoxes-2) {
+
+                // in this case to prevent more boxes being drawn, draw all the leftovers till numberOfBoxes value.
+                numberOfBoxesVisible=3;
+                numberOfBoxesVisible+=numberOfBoxes-pageNumber;
+                parent.style.paddingLeft=((screenWidth/2-20*numberOfBoxesVisible)+'px');
+
+                parent.innerHTML+='<div class="paginationDots">...</div>';
+
+                for(var i=pageNumber; i<=numberOfBoxes; i++) {
+                    parent.innerHTML+='<div class="pageNumberBox" onclick=buildLeaderboard("'+tableID+'",listPeople,'+n+','+m+','+(i-1)+','+itemsPerPage+',"'+pageNumberContainerId+'");>'+(i)+'</div>';
+                }
+                
+            } else {
+                // the standard case, first two box, then ... and then current-1, current, current+1 box and then finally last two boxes.
+
+                parent.innerHTML+='<div class="paginationDots">...</div>';
+                parent.innerHTML+='<div class="pageNumberBox" onclick=buildLeaderboard("'+tableID+'",listPeople,'+n+','+m+','+(pageNumber-2)+','+itemsPerPage+',"'+pageNumberContainerId+'");>'+(pageNumber-1)+'</div>';
+                parent.innerHTML+='<div class="pageNumberBox" onclick=buildLeaderboard("'+tableID+'",listPeople,'+n+','+m+','+(pageNumber-1)+','+itemsPerPage+',"'+pageNumberContainerId+'");>'+(pageNumber)+'</div>';
+                parent.innerHTML+='<div class="pageNumberBox" onclick=buildLeaderboard("'+tableID+'",listPeople,'+n+','+m+','+(pageNumber)+','+itemsPerPage+',"'+pageNumberContainerId+'");>'+(pageNumber+1)+'</div>';
+                parent.innerHTML+='<div class="paginationDots">...</div>';
+                parent.innerHTML+='<div class="pageNumberBox" onclick=buildLeaderboard("'+tableID+'",listPeople,'+n+','+m+','+(numberOfBoxes-1)+','+itemsPerPage+',"'+pageNumberContainerId+'");>'+numberOfBoxes+'</div>';
+            }
+
+        }
+
     }
 
 }
